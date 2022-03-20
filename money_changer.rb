6e1ec7068473623244220fa83327e64f16d66534
moya <moyamauthoor@outlook.com>
2022-03-21 07:52:46 +1100

    Update to coin calculating method

--- money_changer.rb
+++ money_changer.rb
@@ -1,19 +1,21 @@
-def money_changer(change)
+def calculate_coins(change)
     coins = [50, 20, 10, 5, 2, 1]
 
-    coins.each do | coin |
-        if coin <= change
-            coin_quantity = change / coin
-            remainder_change = change % coin 
-            puts "#{coin_quantity} * #{coin} cent coins"
-            change = remainder_change
-        end
-    end 
+    coins
+        .sort().reverse()
+        .each do | coin |
+            if coin <= change
+                coin_quantity = change / coin
+                remainder_change = change % coin 
+                puts "#{coin_quantity} * #{coin} cent coins"
+                change = remainder_change
+            end
+        end 
 end
 
 puts "Enter your change in cents"
 change = gets.chomp.to_i
-money_changer(change)
+calculate_coins(change)
 
 
 
