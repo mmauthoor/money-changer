f70196fb9126e031452f2b2f67e4b35d714bdea2
moya <moyamauthoor@outlook.com>
2022-03-21 09:40:06 +1100

    Add check user input method

--- money_changer.rb
+++ money_changer.rb
@@ -1,8 +1,28 @@
+def check_user_input(input)
+    if input.strip.empty?
+        raise ArgumentError.new("No value provided - program requires an integer.")
+    end
+    
+    if input.to_i.zero?
+        raise ArgumentError.new("Incorrect data type - program requires an integer, not a string.")
+    end
+
+    # Argument error will be thrown here if the input is a float/decimal value
+    change = Integer(input)
+
+    if change.negative?
+        raise ArgumentError.new("Negative value provided - program requires a positive integer.")        
+    end
+
+    calculate_coins(change)
+end
+
+
 def calculate_coins(change)
     coins = [50, 20, 10, 5, 2, 1]
 
     coins
-        .sort().reverse()
+        .sort.reverse
         .each do | coin |
             if coin <= change
                 coin_quantity = change / coin
@@ -14,9 +34,8 @@ def calculate_coins(change)
 end
 
 puts "Enter your change in cents"
-change = gets.chomp.to_i
-calculate_coins(change)
-
+input = gets.chomp
+check_user_input(input)
 
 
 
